f442076ed701cba0e662469e8121fcccc1bf5e19
jorge ruivinho <jorge.ruivinho@me.com>
2017-08-11 18:16:02 +0200

    cocktail possibility to delete

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def image_url_with_default photo, options
+    if photo.nil?
+      "https://image.shutterstock.com/display_pic_with_logo/172806368/641116756/stock-vector-cocktail-icon-cocktail-icon-vector-cocktail-drink-641116756.jpg"
+    else
+      cl_image_path photo.path, options
+    end
+  end
 end
