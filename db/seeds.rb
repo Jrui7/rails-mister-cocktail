d107c991f3ba09c529cec10d00a96d7cf4d36f53
jorge ruivinho <jorge.ruivinho@me.com>
2017-08-11 12:08:43 +0200

    modif seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,10 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Ingredient.create(name: "lemon")
-Ingredient.create(name: "ice")
-Ingredient.create(name: "mint leaves")
+
+Ingredient.create(name: "Rhum")
+Ingredient.create(name: "Vodka")
+Ingredient.create(name: "beer")
+Ingredient.create(name: "Limocelo")
+Ingredient.create(name: "whisky")
+Ingredient.create(name: "porto")
